3a214d4607d7b6abce3b831080de89c7cd894e2e
Jadilson Guedes <jadilson12@gmail.com>
2019-11-09 12:07:46 -0300

    [ADD] Implementa a navBar

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule ,NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-
     NgbModule,
-    NgbAlertModule,
-
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
