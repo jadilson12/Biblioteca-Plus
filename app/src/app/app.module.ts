5b3dbea30654994300fdd6fd0ab5029632520914
Jadilson Guedes <jadilson12@gmail.com>
2019-11-09 11:40:27 -0300

    [ADD] Implementa o bootstrap

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule ,NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
+    NgbModule,
+    NgbAlertModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
