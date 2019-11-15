3be0d9814439eed70c22467a586e0743559abb1e
Jadilson Guedes <jadilson12@gmail.com>
2019-11-15 16:00:36 -0300

    [UPDATE] Atualizando os End-point  padrões rest

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,24 +4,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Categoria'], function (){
     Route::get('/categorias', 'CategoriaController@get');
-    Route::post('/categoria', 'CategoriaController@post');
-    Route::get('/categoria/{id}', 'CategoriaController@show');
-    Route::put('/categoria/{id}', 'CategoriaController@put');
-    Route::delete('/categoria/{id}', 'CategoriaController@delete');
+    Route::post('/categorias', 'CategoriaController@post');
+    Route::get('/categorias/{id}', 'CategoriaController@show');
+    Route::put('/categorias/{id}', 'CategoriaController@put');
+    Route::delete('/categorias/{id}', 'CategoriaController@delete');
 });
 
 Route::group(['namespace' => 'Livro'], function (){
     Route::get('/livros', 'LivroController@get');
-    Route::post('/livro', 'LivroController@post');
-    Route::get('/livro/{id}', 'LivroController@show');
-    Route::put('/livro/{id}', 'LivroController@put');
-    Route::delete('/livro/{id}', 'LivroController@delete');
+    Route::post('/livros', 'LivroController@post');
+    Route::get('/livros/{id}', 'LivroController@show');
+    Route::put('/livros/{id}', 'LivroController@put');
+    Route::delete('/livros/{id}', 'LivroController@delete');
 });
 
 Route::group(['namespace' => 'User'], function (){
     Route::get('/users', 'UserController@get');
-    Route::post('/user', 'UserController@post');
-    Route::get('/user/{id}', 'UserController@show');
-    Route::put('/user/{id}', 'UserController@put');
-    Route::delete('/user/{id}', 'UserController@delete');
+    Route::post('/users', 'UserController@post');
+    Route::get('/users/{id}', 'UserController@show');
+    Route::put('/users/{id}', 'UserController@put');
+    Route::delete('/users/{id}', 'UserController@delete');
 });
